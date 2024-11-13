fa1be1e672aa41927a8fedaf971d3f5e8178136d
John Doe <johndoe@example.com>
2024-11-13 20:05:01 +0300

    feat: установили zustand

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Aboutpage from './pages/Aboutpage';
 
+
+
 function App() {
   return (
     <div>
