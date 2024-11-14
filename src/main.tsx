6a46df599d5f8809567157961b393decac82ebd5
John Doe <johndoe@example.com>
2024-11-15 00:57:24 +0300

    feat: встроили DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
      <BrowserRouter>
          <App />
        </BrowserRouter>
+       <ReactQueryDevtools initialIsOpen={false} />
        </QueryClientProvider>
   </StrictMode>,
 )
